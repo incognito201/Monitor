38d4804c68d5add88551c80e44b174d3d09e89af
Incógnito <incognitosenpai860@gmail.com>
2022-05-04 08:49:03 -0300

    Adição de código útil para trabalhar com reflexão.

---
+++ src/Monitor/Utils/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Monitor.Utils
+{
+    public static class ObjectExtension
+    {
+        public static object GetPropertyValue(this object obj, string propertyName)
+        {
+            return obj
+                .GetType()
+                .GetProperties()
+                .Single(pi => pi.Name == propertyName)
+                .GetValue(obj, null);
+        }
+    }
+}
---
+++ src/Monitor/Utils/PropertyExpressionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Monitor.Utils
+{
+    public static class PropertyExpressionExtension
+    {
+        public static string GetPropertyName<TSource, TProperty>(this Expression<Func<TSource, TProperty>> propertyLambda)
+        {
+            return PropertyUtil.GetPropertyInfo(propertyLambda).Name;
+        }
+
+        public static MemberInfo GetMemberInfo<T>(this Expression<Func<T, object>> exp)
+        {
+            var member = exp.Body as MemberExpression;
+            return (member ?? (exp.Body is UnaryExpression unary ? unary.Operand as MemberExpression : null)).Member;
+        }
+    }
+}
