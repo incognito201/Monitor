7c73774dacbd3dbc7f62500bb6a68fbf213a96a0
Incógnito <incognitosenpai860@gmail.com>
2022-05-04 09:03:54 -0300

    Adicionando wrappers para EventArgs.

---
+++ src/Monitor/Collection/ItemReplacedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Monitor.Collection
+{
+    public class ItemReplacedEventArgs<T> : EventArgs
+    {
+        public ItemReplacedEventArgs(T newItem, T oldItem)
+        {
+            NewItem = newItem;
+            OldItem = oldItem;
+        }
+
+        public T NewItem { get; }
+        public T OldItem { get; }
+    }
+}
